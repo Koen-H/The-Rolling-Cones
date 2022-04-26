5a5099f9c3d083037603e96cc492f520d24a6a64
WiebeHero <wiebesportel2001@hotmail.com>
2022-04-26 14:30:19 +0200

    Patch test

--- Tank Game/Tank_game/PhysicsEngine/Colliders/LineCollider.cs
+++ Tank Game/Tank_game/PhysicsEngine/Colliders/LineCollider.cs
@@ -90,7 +90,7 @@ namespace GXPEngine.PhysicsEngine.Colliders
                 if (MyGame.collisionManager.FirstTime)
                 {
                     Vec2 velocity = ball.Velocity;
-                    velocity.Reflect(info.normal, 0.2F);
+                    velocity.Reflect(info.normal, 0.3F);
                     ball.Velocity = velocity;
                 }
             }
