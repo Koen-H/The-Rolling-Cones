d05cdfebc53d95d577c9f0a53ac64699f5cada59
WiebeHero <wiebesportel2001@hotmail.com>
2022-04-26 12:02:17 +0200

    Patch 0.01
    + Tiled additions

---
+++ Tank Game/Tank_game/Golgrath/Levels/LevelManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine.Golgrath.Levels
+{
+    public class LevelManager : GameObject
+    {
+        private List<Level> levels;
+        public LevelManager()
+        {
+            this.levels = new List<Level>();
+            this.levels.Add(new LevelBottom());
+            this.SetActiveLevel(this.levels[0].GetIdentifier());
+        }
+
+        public void SetActiveLevel(string levelIdentifier)
+        {
+            if (this.levels.Count != 0)
+            {
+                foreach (Level level in this.levels)
+                {
+                    if (level.GetIdentifier().Equals(levelIdentifier))
+                    {
+                        if (this.GetChildCount() != 0)
+                        {
+                            this.GetChildren()[0].Remove();
+                        }
+                        level.Construct();
+                        AddChild(level);
+                    }
+                }
+            }
+        }
+        public Level GetActiveLevel()
+        {
+            return (Level)this.GetChildren()[0];
+        }
+        public Level GetLevel(int index)
+        {
+            if (index < this.levels.Count && index >= 0)
+            {
+                return this.levels[index];
+            }
+            return null;
+        }
+        public Level GetLevel(string levelIdentifier)
+        {
+            if (this.levels.Count != 0)
+            {
+                foreach (Level level in this.levels)
+                {
+                    if (level.GetIdentifier().Equals(levelIdentifier))
+                    {
+                        return level;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Tank Game/Tank_game/Golgrath/Tiles/Tile.cs
@@ -0,0 +1,28 @@
+﻿using GXPEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine.Golgrath.Tiles
+{
+    public class Tile : AnimationSprite
+    {
+        private string identifier;
+
+        public Tile(string tileAsset, int column, int row) : base(tileAsset, column, row, -1, false, true)
+        {
+            this.identifier = tileAsset.Split('.')[0];
+            this.collider.isTrigger = true;
+        }
+
+        public string GetIdentifier()
+        {
+            return this.identifier;
+        }
+        public void SetIdentifier(string identifier)
+        {
+            this.identifier = identifier;
+        }
+    }
+}
