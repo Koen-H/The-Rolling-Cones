6ebd6733ee042709cc0655c174bdec2d0a19287d
WiebeHero <wiebesportel2001@hotmail.com>
2022-04-26 14:39:19 +0200

    Test

--- Tank Game/Tank_game/Golgrath/Objects/PlayerBall.cs
+++ Tank Game/Tank_game/Golgrath/Objects/PlayerBall.cs
@@ -52,7 +52,7 @@ namespace GXPEngine.Golgrath.Objects
             Fill(red, green, blue);
             Stroke(red, green, blue);
             Rect(radius, radius * 2, radius / 2, radius / 2);
-            //Test
+            //Testsss
         }
 
         public new void Step()
