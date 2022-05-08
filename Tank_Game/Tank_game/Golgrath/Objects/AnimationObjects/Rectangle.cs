e5f850b96bb8335e886d78a36064dda9498fd51d
WiebeHero <wiebesportel2001@hotmail.com>
2022-05-08 12:25:12 +0200

    Patch 0.12
    * A bug fix

--- Tank_Game/Tank_game/Golgrath/Objects/AnimationObjects/Rectangle.cs
+++ Tank_Game/Tank_game/Golgrath/Objects/AnimationObjects/Rectangle.cs
@@ -20,6 +20,17 @@ namespace GXPEngine.Golgrath.Objects.AnimationObjects
             this.leftSide = new CanvasLine(sides[3], sides[0], 0xffffffff, false, false, true);
         }
 
+        public Rectangle(Vec2 position, string filename, int columns, int rows, int frames) : base(position, filename, columns, rows, frames)
+        {
+            this.myCollider = new SquareCollider(this);
+            MyGame.collisionManager.AddCollider(this.myCollider);
+            Vec2[] sides = { new Vec2(position.x, position.y), new Vec2(position.x + width, position.y), new Vec2(position.x + width, position.y + height), new Vec2(position.x, position.y + height) };
+            this.topSide = new CanvasLine(sides[0], sides[1], 0xffffffff, false, false, true);
+            this.rightSide = new CanvasLine(sides[1], sides[2], 0xffffffff, false, false, true);
+            this.bottomSide = new CanvasLine(sides[2], sides[3], 0xffffffff, false, false, true);
+            this.leftSide = new CanvasLine(sides[3], sides[0], 0xffffffff, false, false, true);
+        }
+
         public Rectangle(Vec2 position, string filename, int width, int height, int columns, int rows) : base(position, filename, width, height, columns, rows)
         {
             this.myCollider = new SquareCollider(this);
