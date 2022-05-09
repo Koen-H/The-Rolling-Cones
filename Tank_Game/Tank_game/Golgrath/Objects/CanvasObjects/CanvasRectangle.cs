04dff987b98f942849473dc419baa6800cb132fd
KoendertNL <49477924+KoendertNL@users.noreply.github.com>
2022-05-09 13:54:21 +0200

    Tiled v1

--- Tank_Game/Tank_game/Golgrath/Objects/CanvasObjects/CanvasRectangle.cs
+++ Tank_Game/Tank_game/Golgrath/Objects/CanvasObjects/CanvasRectangle.cs
@@ -15,10 +15,10 @@ namespace GXPEngine.Golgrath.Objects
             this.myCollider = new SquareCollider(this);
             MyGame.collisionManager.AddCollider(this.myCollider);
             Vec2[] sides = { new Vec2(position.x, position.y), new Vec2(position.x + width, position.y), new Vec2(position.x + width, position.y + height), new Vec2(position.x, position.y + height) };
-            this.topSide = new CanvasLine(sides[0], sides[1], 0xffffffff, false, false, true);
+            /*this.topSide = new CanvasLine(sides[0], sides[1], 0xffffffff, false, false, true);
             this.rightSide = new CanvasLine(sides[1], sides[2], 0xffffffff, false, false, true);
             this.bottomSide = new CanvasLine(sides[2], sides[3], 0xffffffff, false, false, true);
-            this.leftSide = new CanvasLine(sides[3], sides[0], 0xffffffff, false, false, true);
+            this.leftSide = new CanvasLine(sides[3], sides[0], 0xffffffff, false, false, true);*/
         }
 
         private void Draw(byte red, byte green, byte blue)
